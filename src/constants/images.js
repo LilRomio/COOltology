5d28ffcb2aa67ba7825fd9b58cd4aaf74def75db
LilRomio <popescu.romeo9@gmail.com>
2024-06-01 10:49:21 +0300

    first push

---
+++ src/constants/images.js
@@ -0,0 +1,9 @@
+import logo from '../assets/logo.png';
+import logo2 from '../assets/logo2.svg';
+import headerImg from '../assets/beautiful-rendering-colorful-circles.jpg';
+
+export default {
+  logo,
+  logo2,
+  headerImg,
+};
---
+++ src/container/Values/Values.jsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import './Values.scss';
+
+const Values = () => {
+  return <div>Values</div>;
+};
+
+export default Values;
