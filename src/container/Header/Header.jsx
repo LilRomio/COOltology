f1164631148769a2e15d7684087b8bef9639a3c4
LilRomio <popescu.romeo9@gmail.com>
2024-06-04 21:05:46 +0300

    navigation and wrapper

--- src/container/Header/Header.jsx
+++ src/container/Header/Header.jsx
@@ -2,6 +2,7 @@ import React, { useState, useEffect } from 'react';
 import { motion } from 'framer-motion';
 import { images } from '../../constants';
 import TrackVisibility from 'react-on-screen';
+import { AppWrap } from '../../wrapper';
 import './Header.scss';
 
 const Header = () => {
@@ -77,4 +78,4 @@ const Header = () => {
   );
 };
 
-export default Header;
+export default AppWrap(Header, 'home');
--- src/container/Skills/Skills.jsx
+++ src/container/Skills/Skills.jsx
@@ -1,8 +1,15 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import { AiFillEye } from 'react-icons/ai';
+import { motion } from 'framer-motion';
+import { AppWrap } from '../../wrapper';
 
 import './Skills.scss';
 const Skills = () => {
-  return <div>Skills</div>;
+  return (
+    <>
+      <h2 className="head-text">What we do.. </h2>
+    </>
+  );
 };
 
-export default Skills;
+export default AppWrap(Skills, 'what we do');
