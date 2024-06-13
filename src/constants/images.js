510157149df2e55912c35e587fb57f30e071897c
LilRomio <popescu.romeo9@gmail.com>
2024-06-13 15:02:14 +0300

    we do section

--- src/constants/images.js
+++ src/constants/images.js
@@ -5,6 +5,7 @@ import headerImg2 from '../assets/abstract-modern-3d-geometric-minimalist-shape.
 import profileImg1 from '../assets/close-up-beautiful-optical-fiber-details.jpg';
 import profileImg2 from '../assets/close-up-light-trails.jpg';
 import profileImg3 from '../assets/top-view-colorful-translucent-geometric-shapes.jpg';
+import profileImg4 from '../assets/beautiful-rendering-colorful-circles.jpg';
 import founder from '../assets/Sandra.jpg';
 import mountains from '../assets/mountains.png';
 import stars from '../assets/stars.png';
@@ -20,6 +21,7 @@ export default {
   profileImg1,
   profileImg2,
   profileImg3,
+  profileImg4,
   founder,
   mountains,
   stars,
--- src/container/WhatWeDo/WhatWeDo.jsx
+++ src/container/WhatWeDo/WhatWeDo.jsx
@@ -7,55 +7,83 @@ const items = [
   {
     id: 1,
     title: 'Brand Strategic Planning',
-    description: 'Vision-driven Roadmap ',
+    subtitle: 'Vision-driven Roadmap ',
+    description1: 'Brand Audit',
+    description2: 'Brand Insights',
+    description3: ' Actionable Roadmap to prioritize initiatives and optimize return',
   },
   {
     id: 2,
     title: 'Brand Delivery ',
-    description: 'Tactics. Supervision. Guidance. ',
+    subtitle: 'Tactics. Supervision. Guidance. ',
+    description1: 'Maintenance & Supervision of Brand Strategy ',
+    description2: 'Creation of processes for brand implementation, both internally and externally',
+    description3:
+      ' Optimize every touchpoint where your brand will be seen and experienced, both internally and externally.',
   },
   {
     id: 3,
     title: 'Culture & Momentum Analysis',
-    description: 'The heartbeat of brand behavior. ',
+    subtitle: 'The heartbeat of brand behavior ',
+    description1: "Understanding your brand's unique identity and amplifying its impact.",
+    description2:
+      'Analysis of the brand’s pulse when it comes to the sociological and emotional status your target audience finds themselves in. ',
+    description3: 'Actionable guidance for a better brand journey. ',
   },
   {
     id: 4,
     title: 'Brand Review Workshop ',
-    description: 'Comprehensive overview of brand essence. ',
+    subtitle: 'Comprehensive overview of brand essence. ',
+    description1:
+      'Extended one day workshops offering a 360 Brand Pillars Analysis in order to unfold the brand’s strategy for scaling or launch. ',
+    description2:
+      'An in-depth session designed to uncover the strengths and opportunities within your brand and its current resonance with the world.',
   },
 ];
 
 const WhatWeDo = () => {
   return (
     <>
-      <div className="weDo__wrapper">
-        <div className="weDo__title ">
-          <h1 className="head-text weDo-title">We Do</h1>
+      <div className="container">
+        <div className="weDo-info">
+          <h1 className="p-text weDo-title">We Do</h1>
         </div>
 
-        <div className="app__cards">
+        <div className="weDo-grid">
           {items.map((about, index) => (
             <motion.div
               whileInView={{ opacity: 1 }}
               whileHover={{ scale: 1.1 }}
               transition={{ duration: 0.5, type: 'tween' }}
-              className="app__cards-item"
+              className="weDo-card"
               key={about.title + index}
             >
-              <h2 className="bold-text " style={{ fontWeight: 400, fontSize: '1.2rem' }}>
-                <span className="decoration__about"> // </span> {about.title}
-              </h2>
-              <hr />
               <motion.div
-                className="card__description"
                 whileInView={{ y: [300, 0] }}
                 transition={{ duration: 1, ease: 'easeOut' }}
+                className="bold-text "
+                style={{ fontWeight: 400, fontSize: '1.8rem' }}
               >
-                <p className="p-text " style={{ marginTop: 10 }}>
-                  {about.description}
-                </p>
+                <span className="decoration__about"> // </span> {about.title}
               </motion.div>
+              <hr />
+              <div className="card__subtitle">
+                <p className="p-text " style={{ marginTop: 10, fontSize: '1.2rem' }}>
+                  {about.subtitle}
+                </p>
+                <br />
+              </div>
+              <div className="card__description">
+                <p className="p-text " style={{ fontSize: '0.8rem' }}>
+                  <span>&#x1F5F9;</span> {about.description1}
+                </p>
+                <p className="p-text " style={{ fontSize: '0.8rem' }}>
+                  <span>&#x1F5F9;</span> {about.description2}
+                </p>
+                <p className="p-text " style={{ fontSize: '0.8rem' }}>
+                  <span>&#x1F5F9;</span> {about.description3}
+                </p>
+              </div>
             </motion.div>
           ))}
         </div>
