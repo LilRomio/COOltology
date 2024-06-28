900ae79923ab09d9660e68164ff707026db840ba
LilRomio <popescu.romeo9@gmail.com>
2024-06-28 09:11:37 +0300

    mobile v4

--- src/components/index.js
+++ src/components/index.js
@@ -1,5 +1,5 @@
 export { default as Navbar } from './Navbar/Navbar';
-export { default as Logo } from './Logo/Logo';
+
 export { default as NavigationDots } from './NavigationDots';
 export { default as Cursor } from './Cursor/Cursor';
 export { default as PrivacyPolicy } from './PrivacyPolicy';
