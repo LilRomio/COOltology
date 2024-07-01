fd44f60b805d427a3f538f90564579a662cf6eaf
LilRomio <popescu.romeo9@gmail.com>
2024-07-01 14:06:52 +0300

    button mobile

--- src/components/Button/index.jsx
+++ src/components/Button/index.jsx
@@ -30,22 +30,25 @@ export default function Button({ isActive, toggleMenu }) {
         animate={{ top: isActive ? '-100%' : '0%' }}
         transition={{ duration: 0.5, type: 'tween', ease: [0.76, 0, 0.24, 1] }}
       >
-        <div
-          className={styles.el}
-          onClick={() => {
-            toggleMenu();
-          }}
-        >
-          <PerspectiveText label="Menu" />
-        </div>
-        <div
-          className={styles.el}
-          onClick={() => {
-            toggleMenu();
-          }}
-        >
-          <PerspectiveText label="Close" />
-        </div>
+        {isActive ? (
+          <div
+            className={styles.el}
+            onClick={() => {
+              toggleMenu();
+            }}
+          >
+            <PerspectiveText label="Close" />
+          </div>
+        ) : (
+          <div
+            className={styles.el}
+            onClick={() => {
+              toggleMenu();
+            }}
+          >
+            <PerspectiveText label="Menu" />
+          </div>
+        )}
       </motion.div>
     </div>
   );
