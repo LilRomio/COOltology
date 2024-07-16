292620b69a33c72c762cbcbde504b12783307751
LilRomio <popescu.romeo9@gmail.com>
2024-07-16 12:04:59 +0300

    bugs fix 1

--- src/constants/images.js
+++ src/constants/images.js
@@ -1,41 +1,18 @@
 import logoBlack from '../assets/logo.svg';
 import logoWhite from '../assets/logo-white.svg';
-import headerImg from '../assets/purple-abstract-background-element.jpg';
+
 import headerImg2 from '../assets/geometrical-wallpaper-with-minimalist-shapes.jpg';
-import profileImg1 from '../assets/close-up-beautiful-optical-fiber-details.jpg';
-import profileImg2 from '../assets/close-up-light-trails.jpg';
-import profileImg3 from '../assets/top-view-colorful-translucent-geometric-shapes.jpg';
-import profileImg4 from '../assets/beautiful-rendering-colorful-circles.jpg';
+
 import founder from '../assets/Sandra.jpg';
-import mountains from '../assets/mountains.png';
-import stars from '../assets/stars.png';
-import sun from '../assets/sun.png';
-import planets from '../assets/planets.png';
-import people from '../assets/people.webp';
-import whiteBg1 from '../assets/geometrical-wallpaper-with-minimalist-shapes.jpg';
-import whiteBg2 from '../assets/modern-background-with-white-round-lines.jpg';
-import whiteBg3 from '../assets/modern-background-with-white-shapes.jpg';
+
 import Arrow from '../assets/arrow.svg';
 import Symbol from '../assets/logo-icon.svg';
 
 export default {
   logoBlack,
   logoWhite,
-  headerImg,
   headerImg2,
-  profileImg1,
-  profileImg2,
-  profileImg3,
-  profileImg4,
   founder,
-  mountains,
-  stars,
-  sun,
-  planets,
-  people,
-  whiteBg1,
-  whiteBg2,
-  whiteBg3,
   Arrow,
   Symbol,
 };
